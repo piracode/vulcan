c054f235ad3c06e0ed57ae5cd9dfd207994e15a7
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-03-31 12:36:34 -0700

    refactored header

--- dev/scripts/libs/slick.js
+++ dev/scripts/libs/slick.js
@@ -1,38 +1,3 @@
-$(document).ready(function () {
-  $(".slider-wrapper").slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    SlidesToScroll: 1,
-  });
-});
-
-// $(document).ready(function () {
-//   $(".your-class").slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     SlidesToScroll: 3,
-//   });
-// });
-
-// const headerNav = document.getElementById("header-nav");
-
-// headerNav.style.display = "none";
-
-// navigation
-const navigation = document.getElementById("header-nav");
-const menu = document.getElementById("menu");
-const menuUl = document.querySelector(".header__links");
-menu.addEventListener("click", () => {
-  navigation.style.setProperty("--childenNumber", navigation.children.length);
-  navigation.classList.toggle("active");
-  menu.classList.toggle("active");
-  menuUl.classList.toggle("active");
-});
-
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
@@ -3044,5 +3009,3 @@ menu.addEventListener("click", () => {
     };
 
 }));
-
-//# sourceMappingURL=script.min.js.map
